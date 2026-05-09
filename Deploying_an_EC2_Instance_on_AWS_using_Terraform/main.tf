ab5d3f34dead8b6997084b7fe5149b5959f0c08d
Mamun172136 <ma1536204@gmail.com>
2026-05-09 19:25:00 +0600

    add aws security group

--- Deploying_an_EC2_Instance_on_AWS_using_Terraform/main.tf
+++ Deploying_an_EC2_Instance_on_AWS_using_Terraform/main.tf
@@ -7,3 +7,36 @@ resource "aws_key_pair" "web_key"  {
   public_key = file("~/ssh/web_key.pub")
 }
 
+resource "aws_instance" "web_server" {
+  ami           = "ami-060e277c0d4cce553"  # Ubuntu 20.04 AMI ID
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.web_key.key_name
+  subnet_id = "subnet-003b060e207b1ba24"  # Replace with your subnet ID
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+  associate_public_ip_address = true  
+
+  tags = {
+    Name        = "web_server"
+    Description = "An NGINX web server on Ubuntu"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y nginx
+  EOF
+}
+
+resource "aws_security_group" "ssh_access" {
+  name= "ssh access"
+  description = ""
+  vpc=""
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }
+}
