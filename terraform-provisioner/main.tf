c2997402f068712119f33d5d3092de83000f1674
Mamun172136 <ma1536204@gmail.com>
2026-05-16 07:05:39 +0600

    create ec2 with terraform

--- terraform-provisioner/main.tf
+++ terraform-provisioner/main.tf
@@ -2,9 +2,42 @@ provider "aws"{
     region= "ap-southeast-1"
 }
 
-aws "aws-key-pair" "example"{
+resource "aws_key_pair" "example"{
     key_name="example-key"
     public_key=file("~/.ssh/id_rsa.pub)
 }
 
-aws "
+resource "aws_security_group" "example"{
+    vpc_id=""
+
+    ingress{
+        from_port=22
+        to_port=22
+        protocol=tcp
+        cidr_blocks= ["0.0.0.0/0"]
+        
+    }
+
+    ingress{
+        from_port=22
+        to_port=22
+        protocol=tcp
+        cicd_blocks=["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example"{
+    ami=""
+    instance_type=t2.micro
+    key_name= aws_key_pair.example.key_name
+    subnet_id=""
+    vpc_security_groups_ids=[aws_security_group.example.id]
+    associate_public_id_address= true
+}
